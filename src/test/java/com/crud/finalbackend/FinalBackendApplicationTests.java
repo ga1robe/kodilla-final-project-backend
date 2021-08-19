df6e3e666b8fc1d628b84766c68ff59242012c66
Robert Gajewski <robert.gajewski@nask.pl>
2021-08-19 21:05:17 +0200

    first load

---
+++ src/test/java/com/crud/finalbackend/FinalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.finalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
